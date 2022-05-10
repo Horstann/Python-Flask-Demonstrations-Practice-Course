34c7ea744e4c976bdf9af15d628d10e823c40390
Horstann <horstannho@gmail.com>
2022-05-10 14:20:02 +0800

    Started 2nd half of course on Flask Framework Demonstrations!

---
+++ Flask Framework Demonstrations/application.py
@@ -0,0 +1,24 @@
+from flask import Flask, render_template
+app = Flask(__name__)
+
+@app.route("/")
+def index():
+    return "Hello world!"
+
+if __name__ == "__main__":
+    app.run(debug=True)
+    # once app launched to production environment, MUST set 'debug=False'
+
+"""
+To set up virtual environment, type this in terminal:
+
+# Create virtual environment, which name is "env"
+virtualenv env
+# Activate "env"
+source env\Scripts\activate.bat
+# Provide environment variables
+export FLASK_APP=application.py
+export FLASK_DEBUG=1
+# Run application
+flask run
+"""
